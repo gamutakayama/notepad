b8eb92d78ff71e35efea1d1fab4b3e96230a1c88
WoMeng <github@wome.ng>
2025-07-02 13:42:51 +0800

    ...

--- src/login.php
+++ src/login.php
@@ -81,8 +81,8 @@ function sendMessage($success)
 
 function getClientIP()
 {
-  if (!empty($_SERVER["HTTP_CLIENT_IP"])) {
-    return $_SERVER["HTTP_CLIENT_IP"];
+  if (!empty($_SERVER["HTTP_CF_CONNECTING_IP"])) {
+    return $_SERVER["HTTP_CF_CONNECTING_IP"];
   }
 
   if (!empty($_SERVER["HTTP_X_FORWARDED_FOR"])) {
